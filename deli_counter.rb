929be3a94a5daacc047b98f5bf0e111cd2a42d16
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-04 03:40:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    line = katz_deli.map.with_index {|name, index| [index + 1, name].join(. )}.join( )
+    line = katz_deli.map.with_index {|name, index| [index + 1, name].join(". ")}.join(" ")
     puts "The line is currently".concat(line)
   end
 end
