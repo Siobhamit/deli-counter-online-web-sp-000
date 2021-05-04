bcdce9271bfc909f34445f652fa305c7ee700b01
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-04 03:17:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  while katz_deli = []
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently katz_deli.map {|name| (index + 1). name }"
+  end
+end
+
+def take_a_number(katz_deli, person)
