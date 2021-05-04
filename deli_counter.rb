59561a5ffd9aa31a9060bff95c6679bd3d8827fd
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-04 03:53:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,12 @@ def take_a_number(katz_deli, name)
   spot = katz_deli.index(name) + 1
   puts "Welcome, #{name}. You are number #{spot} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
+  end
+end
